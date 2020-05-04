657886db9ad2502a0480f49729090da15b536802
minciu <stefan_minciuna@yahoo.com>
2020-05-04 14:23:59 +0300

    Add files via upload

--- ClientApplication/src/clientapplication/GameClient.java
+++ ClientApplication/src/clientapplication/GameClient.java
@@ -5,6 +5,7 @@
  */
 package clientapplication;
 
+import clientapplication.Gomoku.Board;
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
@@ -13,6 +14,8 @@ import java.util.Scanner;
  * @author user
  */
 public class GameClient {
+    private static char token;
+    private static char opponentToken;
     public GameClient() throws IOException {
     String serverAddress = "127.0.0.1"; // The server's IP address
  int PORT = 8100; // The server's port
@@ -27,7 +30,8 @@ new PrintWriter(socket.getOutputStream(), true);
  new InputStreamReader(socket.getInputStream())) ) {
  // Send a request to the server
   
-   while(true){
+ boolean flag=true;
+   while(flag){
   String  request = myObj.nextLine();
   
     
@@ -36,12 +40,92 @@ new PrintWriter(socket.getOutputStream(), true);
  // Wait the response from the server 
  String response = in.readLine ();
  System.out.println(response);
- if(response.equals("exit")) break;
- }} catch (UnknownHostException e) {
+ // Exit the loop
+ if(response.equals("exit")) {
+     flag=false;
+     break;
+  } else if (response.contains("created") || response.contains("joined")) {
+                    setToken(response);
+                    gameMode(in, out);
+                }
+ 
+ }
+ } catch (UnknownHostException e) {
  System.err.println("No server listening... " + e);
  }
-     
-        
+      
     }
+    private static String getCommand() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Send a new command: ");
+        return scanner.nextLine();
     }
+    
+    private static void setToken(String response) {
+        if (response.contains("create")) {
+            token = 'x';
+            opponentToken = 'o';
+        } else {
+            token = 'o';
+            opponentToken = 'x';
+        }
+    }
+    
+    private static void gameMode(BufferedReader in, PrintWriter out) throws IOException {
+        Board board = new Board();
+        String response = in.readLine();
+        System.out.println(response);
+
+        // second player
+        if (token == 'o') {
+            updateBoard(board, response, opponentToken);
+            board.print();
+        }
+
+        boolean playing = true;
+        while (playing) {
+            if (response.contains("won") || response.contains("lost")) {
+                playing = false;
+            } else {
+                String move = "";
+                boolean validMove = false;
+                while (!validMove) {
+                    move = submitMove();
+                    out.println(move);
+
+                    response = in.readLine();
+                    System.out.println(response);
+                    if (!response.contains("Invalid")) {
+                        validMove = true;
+                    }
+                }
+
+                updateBoard(board, move, token);
+                board.print();
+
+                if (response.contains("won") || response.contains("lost")) {
+                    playing = false;
+                } else {
+                    response = in.readLine();
+                    System.out.println(response);
+                    updateBoard(board, response, opponentToken);
+                    board.print();
+                }
+            }
+        }
+    }
+
+    private static String submitMove() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("submit-move ");
+        return scanner.nextLine();
+    }
+
+    private static void updateBoard(Board board, String move, char piece) {
+        String[] arguments = move.split(" ");
+        int x = Integer.parseInt(arguments[arguments.length - 2]);
+        int y = Integer.parseInt(arguments[arguments.length - 1]);
+        board.setPiece(x, y, piece);
+    }
+ }
 
